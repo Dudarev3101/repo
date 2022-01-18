fae15ff8690e8749fafb0d45727efe449351cd42
Dudarev31 <44091792+Dudarev3101@users.noreply.github.com>
2022-01-18 20:28:50 +0300

    Add files via upload

---
+++ qwe.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Янв 18 2022 г., 20:27
+-- Версия сервера: 5.6.51
+-- Версия PHP: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `qwe`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `sorevnovaniya`
+--
+
+CREATE TABLE `sorevnovaniya` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `результаты`
+--
+
+CREATE TABLE `результаты` (
+  `id` int(11) NOT NULL,
+  `имя спортсмена` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `соревнование` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `призер` varchar(3) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `спортсмены`
+--
+
+CREATE TABLE `спортсмены` (
+  `id` int(11) NOT NULL,
+  `name_sportsmena` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `результаты`
+--
+ALTER TABLE `результаты`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
